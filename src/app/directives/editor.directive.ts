240c785164e535e8e2b562d0e8fdcf5d5df4c6be
Murat Eren Nar <muraterennar@hotmail.com>
2023-01-10 01:52:32 +0300

    10.01.2023 Commit 1

--- src/app/directives/editor.directive.ts
+++ src/app/directives/editor.directive.ts
@@ -8,12 +8,6 @@ export class EditorDirective implements OnInit {
     private _renderer: Renderer2,
     private element: ElementRef
   ) {
-    // const btn: HTMLButtonElement = _renderer.createElement('button');
-    // btn.classList.add('btn');
-    // btn.classList.add('btn-outline-success');
-    // btn.innerHTML = 'Deneme';
-    // btn.id = "deneme";
-    // _renderer.appendChild(element.nativeElement, btn);
     this.cretateToolBar();
     this.createParagraph(this.toolBar);
     this.createHeader(this.toolBar);
@@ -24,7 +18,9 @@ export class EditorDirective implements OnInit {
   }
 
   ngOnInit(): void {
-
+    this.clickParagraph(this.paragraphBtn);
+    this.clickHeading();
+    this.clickImage();
   }
 
   // ___ Create Element Button Property ___
@@ -36,7 +32,13 @@ export class EditorDirective implements OnInit {
   italicBtn: HTMLButtonElement;
   codeBtn: HTMLButtonElement;
 
+  // ____ Create Realtime Elements ____
+  editor: HTMLElement;
+  pTag: HTMLParagraphElement;
+  hTag: HTMLHeadingElement;
+  imageTag: HTMLImageElement;
 
+  // Create Toolbar buttons
   cretateToolBar() {
     this.toolBar = this._renderer.createElement(CustomCreateElements.div)
     this.toolBar.classList.add('container');
@@ -80,6 +82,7 @@ export class EditorDirective implements OnInit {
     for (let i = 1; i <= limit; i++) {
       let hButton: HTMLButtonElement = this._renderer.createElement(CustomCreateElements.btn);
       hButton.type = 'button';
+      hButton.id = 'header' + i.toString();
       hButton.classList.add("custom-btn");
       hButton.classList.add("w-100");
       hButton.innerHTML = CustomElementIcon.header + i;
@@ -182,6 +185,7 @@ export class EditorDirective implements OnInit {
     // div => ul => form => col-3 => btn
     let iBtn: HTMLButtonElement = this._renderer.createElement(CustomCreateElements.btn);
     iBtn.type = CustomCreateElements.btn;
+    iBtn.id = 'imageAddBtn'
     iBtn.innerHTML = "Ekle";
     iBtn.classList.add("btn");
     iBtn.classList.add("bg-dark-subtle");
@@ -217,9 +221,145 @@ export class EditorDirective implements OnInit {
 
     element.appendChild(this.codeBtn);
   }
+
+  // Create Editor Tag
+
+  clickParagraph(htmlElement: HTMLElement) {
+    htmlElement.addEventListener('click', () => {
+      this.editor = document.getElementById(CustomCreateElements.editor);
+      // Cretate <p>
+      this.pTag = this._renderer.createElement(CustomCreateElements.p);
+      this.pTag.innerHTML = 'Paragraf';
+      this.pTag.contentEditable = 'true';
+      this.pTag.style.padding = '5px';
+      this.editor.appendChild(this.pTag);
+      console.log(this.pTag);
+    });
+
+
+  }
+
+  clickHeading() {
+    this.editor = document.getElementById(CustomCreateElements.editor);
+    // Create H1
+    let h1: HTMLElement = document.getElementById('header1');
+    h1.addEventListener('click', () => {
+      this.hTag = this._renderer.createElement('h1');
+      this.hTag.innerHTML = 'Başlık',
+        this.hTag.contentEditable = 'true';
+      this.editor.appendChild(this.hTag);
+    });
+
+    // Create H2
+    let h2: HTMLElement = document.getElementById('header2');
+    h2.addEventListener('click', () => {
+      this.hTag = this._renderer.createElement('h2');
+      this.hTag.innerHTML = 'Başlık',
+        this.hTag.contentEditable = 'true';
+      this.editor.appendChild(this.hTag);
+    });
+
+    // Create H3
+    let h3: HTMLElement = document.getElementById('header3');
+    h3.addEventListener('click', () => {
+      this.hTag = this._renderer.createElement('h3');
+      this.hTag.innerHTML = 'Başlık',
+        this.hTag.contentEditable = 'true';
+      this.editor.appendChild(this.hTag);
+    });
+
+    // Create H4
+    let h4: HTMLElement = document.getElementById('header4');
+    h4.addEventListener('click', () => {
+      this.hTag = this._renderer.createElement('h4');
+      this.hTag.innerHTML = 'Başlık',
+        this.hTag.contentEditable = 'true';
+      this.editor.appendChild(this.hTag);
+    });
+
+    // Create H5
+    let h5: HTMLElement = document.getElementById('header5');
+    h5.addEventListener('click', () => {
+      this.hTag = this._renderer.createElement('h5');
+      this.hTag.innerHTML = 'Başlık',
+        this.hTag.contentEditable = 'true';
+      this.editor.appendChild(this.hTag);
+    });
+
+    // Create H6
+    let h6: HTMLElement = document.getElementById('header6');
+    h6.addEventListener('click', () => {
+      this.hTag = this._renderer.createElement('h6');
+      this.hTag.innerHTML = 'Başlık',
+        this.hTag.contentEditable = 'true';
+      this.editor.appendChild(this.hTag);
+    });
+
+  }
+
+  clickImage() {
+
+    let url: any = document.getElementById('floatingInputUrl');
+    let file = document.getElementById('floatingInputFile') as HTMLInputElement;
+    file.files[0];
+    let imageAddBtn: HTMLElement = document.getElementById('imageAddBtn');
+
+    imageAddBtn.addEventListener('click', () => {
+      let urlValue = url.value;
+      url.value = urlValue;
+      // console.log(url)
+
+      if (url.value) {
+        this.imageTag = this._renderer.createElement('img');
+        this.imageTag.id = 'urlImage'
+        this.imageTag.src = url.value;
+        this.imageTag.style.width = '50%';
+        this.imageTag.style.height = 'auto';
+        this.imageTag.style.display = 'block';
+        this.imageTag.style.margin = 'auto';
+        this.imageTag.style.borderRadius = '3px';
+
+        this.editor.appendChild(this.imageTag);
+      }
+      else {
+
+        this.imageTag = this._renderer.createElement('img');
+        this.imageTag.id = 'fileImage';
+        // this.imageTag.src = file.files[0].name;
+        this.imageTag.style.width = '50%';
+        this.imageTag.style.height = 'auto';
+        this.imageTag.style.display = 'block';
+        this.imageTag.style.margin = 'auto';
+        this.imageTag.style.borderRadius = '3px';
+
+
+        // var f: HTMLInputElement | null = document.querySelector('input[type=file]');
+        // f.files[0];
+
+        // debugger;
+        var reader: FileReader = new FileReader();
+        reader.onloadend = () => {
+          this.imageTag.src = reader.result as string;
+        }
+
+        if (file) {
+          reader.readAsDataURL(file.files[0]);
+          console.log(this.imageTag);
+        }
+        else {
+          this.imageTag.src = '';
+          console.log(this.imageTag);
+        }
+
+        this.editor.appendChild(this.imageTag);
+      }
+    });
+
+  }
 }
 
 export enum CustomCreateElements {
+  editor = 'editor',
   btn = 'button',
   div = 'div',
   ul = 'ul',
@@ -228,6 +368,7 @@ export enum CustomCreateElements {
   form = 'form',
   input = 'input',
   label = 'label',
+  p = 'p'
 }
 
 export enum CustomElementIcon {
